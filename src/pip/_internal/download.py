41f51567e8a05c2f42404a82a9b44508d617b6d9
Trishank K Kuppusamy <trishank.kuppusamy@datadoghq.com>
2018-05-18 16:00:07 -0400

    Use Ed25519 for in-toto root layout public keys.

--- src/pip/_internal/download.py
+++ src/pip/_internal/download.py
@@ -226,8 +226,10 @@ class TUFDownloader:
                 # Load the layout and public keys.
                 layout = Metablock.load(layout_relpath)
                 pubkeys = glob.glob('*.pub')
+                # FIXME: Don't hardcode this to neither RSA nor Ed25519. Sniff
+                # from keytype.
                 layout_key_dict = \
-                             import_rsa_public_keys_from_files_as_dict(pubkeys)
+                         import_ed25519_public_keys_from_files_as_dict(pubkeys)
                 # Verify and inspect.
                 params = self.__substitute_parameters(target_relpath)
                 verifylib.in_toto_verify(layout, layout_key_dict,
@@ -331,7 +333,7 @@ if 'TUF_CONFIG_FILE' in os.environ:
 
     from in_toto import verifylib
     from in_toto.models.metadata import Metablock
-    from in_toto.util import import_rsa_public_keys_from_files_as_dict
+    from in_toto.util import import_ed25519_public_keys_from_files_as_dict
     from tuf.client.updater import Updater
 
     tuf_downloader = TUFDownloader(os.environ['TUF_CONFIG_FILE'])
