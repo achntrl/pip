646d40a937182da1be798a86768b677e12700de5
Trishank K Kuppusamy <trishank.kuppusamy@datadoghq.com>
2018-09-04 18:33:19 -0400

    remove obsolete extras

--- setup.py
+++ setup.py
@@ -79,8 +79,8 @@ setup(
 
     zip_safe=False,
     python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
+
     extras_require={
-        'testing': tests_require,
         # NOTE: These are the optional requirements for enabling TUF + in-toto.
         'tuf-in-toto': [
             # At the time of writing (Jun 19 2018), this was the latest version
